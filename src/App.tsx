82f118dfc336718771d2b53e929c4292329ad28e
hima <ebrahumahmed2003@gmail.com>
2025-06-26 04:57:34 +0300

    lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
+import { lazy, Suspense } from "react"
 import { BrowserRouter, Route, Routes } from "react-router"
-import NotFoundPage from "./pages/NotFoundPage"
-import HomePage from "./pages/HomePage"
-import MoviesShowsPage from "./pages/MoviesShowsPage"
-import MoviePage from "./pages/MoviePage"
-import SeriesPage from "./pages/SeriesPage"
-import SupportPage from "./pages/SupportPage"
-import SubscriptionPage from "./pages/SubscriptionPage"
+import LoadingPage from "./state/LoadingPage";
+
+
+const NotFoundPage =lazy(()=> import( "./pages/NotFoundPage"));
+const HomePage =lazy(()=> import( "./pages/HomePage"));
+const MoviesShowsPage =lazy(()=> import( "./pages/MoviesShowsPage"));
+const MoviePage =lazy(()=> import( "./pages/MoviePage"));
+const SeriesPage =lazy(()=> import( "./pages/SeriesPage"));
+const SupportPage =lazy(()=> import( "./pages/SupportPage"));
+const SubscriptionPage =lazy(()=> import( "./pages/SubscriptionPage"));
 
 function App() {
     return (
         <BrowserRouter>
+        <Suspense fallback={<LoadingPage/>} >
         <Routes>
-            <Route index element={<HomePage/>}/>
+            <Route index element={<HomePage />}/>
             <Route path="/support" element={<SupportPage />} />
             <Route path="/subscriptions" element={<SubscriptionPage />}/>
             <Route path="/movies_shows" element={<MoviesShowsPage />}/>
             <Route path="/movies_shows/movie/:id" element={<MoviePage />} />
             <Route path="/movies_shows/series/:id" element={<SeriesPage />} />
-
             <Route path="*" element={<NotFoundPage />} />
         </Routes>
-            
+        </Suspense>
         </BrowserRouter>
     )
 }
---
+++ src/state/LoadingPage.tsx
@@ -0,0 +1,9 @@
+function LoadingPage() {
+    return (
+        <div>
+            Loading...
+        </div>
+    )
+}
+
+export default LoadingPage
