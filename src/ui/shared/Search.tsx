2c1802de8707546b23350c8696e641a4945af3ea
hima <ebrahumahmed2003@gmail.com>
2025-07-17 21:53:58 +0300

    refactor: remove unused useEffect import in Search component

--- src/ui/shared/Search.tsx
+++ src/ui/shared/Search.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import SearchResult from "./SearchResult";
 import { useMovieBySearch, useShowBySearch } from "../../hooks/useSearch";
 import SearchResultShow from "./SearchResultShows";
